80eef76e469d80a2eed6efe36b7dfb9e67427450
聂士伟 <nieshiwei@hywea.com>
2026-09-09 16:28:34 +0800

    chore: 增加语言 enUS 和插件图标；版本升级到 0.1.0

---
+++ Locales/HamsterGather-enUS.lua
@@ -0,0 +1,8 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("HamsterGather", "enUS")
+if not L then return end
+
+L["HamsterGather"] = "HamsterGather"
+-- professions
+L["Fishing"] = "Fishing"
+L["Herbalism"] = "Herbalism"
+L["Mining"] = "Mining"
